873acacc15b9b379e0cc65649726316365343201
Bobur Urinboev <bobur_urinboev@epam.com>
2022-06-17 16:12:22 +0500

    v3 test ec2

---
+++ test_packages/asserts/asserts.py
@@ -0,0 +1,30 @@
+from pytest_check import check_func
+from test_packages.models.aws_instance_models import AWSEC2
+
+"""
+Module with several asserts for test EC2 instances
+"""
+
+
+@check_func
+def soft_check_instance_exist(ec2_instance: AWSEC2) -> bool:
+    """
+    Checks EC2 instance for existence.
+    :param: actual_result: AWSEC2 object
+    :return: return boolean value
+    """
+    error_message = f'EC2 instance does not exist: {ec2_instance.tags_name}'
+    assert ec2_instance, error_message
+    return bool(ec2_instance)
+
+
+def soft_check(expected_result: Dict, actual_result: AWSEC2):
+    """
+    Compares ec2 origin data from aws.amazon.com with expected data from yaml
+    :param expected_result: ec2 expected datas from yaml file
+    :param actual_result: ec2 actual datas from aws servers
+    :return:
+    """
+    error_message = f"expected_result: {expected_result}; " \
+                    f"actual_result: {actual_result}"
+    assert expected_result == actual_result, error_message
---
+++ test_packages/tests/conftest.py
@@ -0,0 +1,1 @@
+from test_packages.fixtures.fixtures_aws_ec2 import *
